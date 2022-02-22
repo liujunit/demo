54c7ed708c35eda3b179b427329834f0a79ad7c7
liujun <156315348@qq.com>
2022-02-22 17:13:22 +0800

    合并测试

--- src/main/test/okhttp/CensysDemo.java
+++ src/main/test/okhttp/CensysDemo.java
@@ -3,6 +3,8 @@ package okhttp;
 import okhttp3.*;
 
 import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
 
 /**
  * 不需要代理也可以，只提供了IP搜索
@@ -18,7 +20,7 @@ public class CensysDemo {
                     String credential = Credentials.basic("5e9db3ce-21ea-42db-a86b-6a71714f1b77", "xSwL7Wtnvmd6Z1NOplwpcLa5rghOKonI");
                     return response.request().newBuilder().header("Authorization", credential).build();
                 })
-//                .proxy(new Proxy(Proxy.Type.SOCKS, new InetSocketAddress("127.0.0.1", 1080)))
+                .proxy(new Proxy(Proxy.Type.SOCKS, new InetSocketAddress("127.0.0.1", 1080)))
                 .build();
         final Request request = new Request.Builder()
                 .url(url1)
---
+++ src/main/test/tcp/GraylogTCPDemo1.java
@@ -0,0 +1,9 @@
+package tcp;
+
+public class GraylogTCPDemo1 {
+
+    public static void main(String[] args) {
+
+    }
+
+}
